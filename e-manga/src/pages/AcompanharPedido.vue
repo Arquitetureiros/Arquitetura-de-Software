bd4b9813823f598cb04d6b9642255f25129e3193
Giovanni Guilioti <giovanni.guilioti@gmail.com>
2022-10-13 22:21:13 -0300

    card estilizado

--- e-manga/src/pages/AcompanharPedido.vue
+++ e-manga/src/pages/AcompanharPedido.vue
@@ -30,19 +30,25 @@
     </q-header>
 
     <q-page-container>
-      <h4>Meus pedidos</h4>
+      <h4 style="text-align: center;">Meus pedidos</h4>
       <q-card class="my-card">
         <q-card-section>
-          <div class="row justify-between">
-            <div class="row">
-              <img src="public/hunterxhunter.jpg" style="height: 140px; max-width: 150px;">
-              <div>
-                <h6>Hunter x Hunter vol. 1</h6>
-                <p>Vendido por Yuripa Mangás</p>
-                <p>Produto entregue em 09/10/2022</p>
+          <div class="row justify-between items-stretch">
+            <div class="row items-center" style="width: 80%;">
+              <div style="width: 20%;" class="flex justify-center">
+                <img src="hunterxhunter.jpg" style="height: 140px;">
+              </div>
+              <div style="width: 60%; height: 100%;" class="flex column">
+                <div style="height: 50%;" class="flex justify-start">
+                  <h6>Hunter x Hunter vol. 1</h6>
+                </div>
+                <div style="width: 50%; height: 50%;" class="flex justify-start items-center">
+                  <p>Vendido por Yuripa Mangás</p>
+                  <p>Produto entregue em 09/10/2022</p>
+                </div>
               </div>
             </div>
-            <div>
+            <div style="width: 20%;" class="flex justify-center items-center">
               <h6>Avaliar Vendedor</h6>
               <q-rating size="2rem" v-model="ratingModel" :max="5" color="yellow" />
             </div>
@@ -51,18 +57,24 @@
       </q-card>
       <q-card class="my-card">
         <q-card-section>
-          <div class="row justify-between">
-            <div class="row">
-              <img src="public/jujutsu-kaisen.jpg" style="height: 140px; max-width: 150px;">
-              <div>
-                <h6>Jujutsu Kaisen vol. 1</h6>
-                <p>Vendido por Yuripa Mangás</p>
-                <p>Pedido em trânsito</p>
+          <div class="row justify-between items-stretch">
+            <div class="row items-center" style="width: 80%;">
+              <div style="width: 20%;" class="flex justify-center">
+                <img src="jujutsu-kaisen.jpg" style="height: 140px;">
+              </div>
+              <div style="width: 60%; height: 100%;" class="flex column">
+                <div style="height: 50%;" class="flex justify-start">
+                  <h6>Jujutsu Kaisen vol. 1</h6>
+                </div>
+                <div style="width: auto; height: auto;" class="flex column justify-between items-start">
+                  <p>Vendido por Yuripa Mangás</p>
+                  <p>Produto em trânsito</p>
+                </div>
               </div>
             </div>
-            <div>
+            <div style="width: 20%;" class="flex justify-center items-center">
               <h6>Avaliar Vendedor</h6>
-              <p>Avaliação disponível após a entrega.</p>
+              <p style="text-align: center;">Avaliação disponível após a entrega ser efetuada</p>
             </div>
           </div>
         </q-card-section>
